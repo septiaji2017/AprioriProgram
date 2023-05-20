a115273b4f2ccd662827e40acb2373e0866baba2
thisisjackii <jaxsix06@gmail.com>
2023-05-20 12:50:58 +0700

    Deleting extraneous comments

--- APR.h
+++ APR.h
@@ -9,30 +9,6 @@
 #define MIN_SUPPORT 2
 #define MIN_CONFIDENCE 0.6
 
-// typedef struct TrieNode {
-//     char* item;
-//     int support;
-//     struct TrieNode* child;
-//     struct TrieNode* sibling;
-// } TrieNode;
-
-// TrieNode* createTrie();
-
-// struct TrieNode *createTrieNode(char* item);
-
-// struct TrieNode *getChildNode(struct TrieNode *node, char* item);
-
-// void addChildNode(struct TrieNode *parent, struct TrieNode *child);
-
-// void readTransaction(struct TrieNode *root, char *transaction);
-
-// void printTrie(struct TrieNode *node, int level);
-
-// void generateFrequentItemsetsFromNode(struct TrieNode* current, struct TrieNode* node, int minSupport);
-
-// void generateFrequentItemsets(struct TrieNode* root, int minSupport);
-
-// typedef struct node *nodeTrie;
 typedef struct nodeTrie {
 	int info;
 	int index;
